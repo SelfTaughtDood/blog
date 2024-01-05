e6cddb0c84710a469d3e1847b2b2a83f05ce88e5
g27sanchez <guiver.sanchez.cohort225@gmail.com>
2024-01-05 12:38:02 -0800

    test commit
    Updated h1 and p tags to reflect change in direction under this branch

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,9 +5,9 @@ import './Header.css'
 function Header() {
   return (
     <header className='header'>
-      <h1>Project Database</h1>
+      <h1>New Project</h1>
       <h2>CRUD App</h2>
-      <p>A simple application built on the M.E.R.N stack, designed to facilitate learning how to build CRUD (Create, Read, Update, Delete) applications.</p>
+      <p>Let's build a website using the infrasture I've built</p>
     </header>
 
     
